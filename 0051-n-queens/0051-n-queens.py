675f6a5a9d600b5396def0c58dc231451d747cd4
thiru29032004 <127879067+thiru29032004@users.noreply.github.com>
2025-05-14 15:11:54 +0530

    Time: 7 ms (97.41%), Space: 18.1 MB (96.34%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,45 +1,31 @@
 class Solution:
-    def isSafe(self, mat, row, col, n):
-    # Check row on left side
-        for c in range(col):
-            if mat[row][c] == 'Q':
-                return False
-
-        # Check upper-left diagonal
-        r, c = row, col
-        while r >= 0 and c >= 0:
-            if mat[r][c] == 'Q':
-                return False
-            r -= 1
-            c -= 1
-
-        # Check lower-left diagonal
-        r, c = row, col
-        while r < n and c >= 0:
-            if mat[r][c] == 'Q':
-                return False
-            r += 1
-            c -= 1
-
-        return True
-
-
-
-    def solve(self,col,mat,n,ans):
+    def solve(self,col,mat,ld,ud,rowarr,ans,n):
         if col==n:
-            ans.append(["".join(row) for row in mat])
-
-            return 
-
+            ans.append([''.join(row) for row in mat])
+            return
         for row in range(n):
-            if self.isSafe(mat,row,col,n):
+            if ld[row+col]==0 and ud[n-1-row+col]==0 and rowarr[row]==0:
                 mat[row][col]='Q'
-                self.solve(col+1,mat,n,ans)
+                ld[row+col]=1
+                ud[n-1-row+col]=1
+                rowarr[row]=1
+                self.solve(col+1,mat,ld,ud,rowarr,ans,n)
                 mat[row][col]='.'
-
+                ld[row+col]=0
+                ud[n-1-row+col]=0
+                rowarr[row]=0
+    
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         mat=[['.']*n for _ in range(n)]
+        ld=[0]*(2*n-1)
+        ud=[0]*(2*n-1)
+        rowarr=[0]*n
         ans=[]
-        self.solve(0,mat,n,ans)
+        self.solve(0,mat,ld,ud,rowarr,ans,n)
         return ans
+
+
+
+
+        
